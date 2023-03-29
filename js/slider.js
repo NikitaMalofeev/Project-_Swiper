4ea3cc2eead6a6db083a2c202769b2bc11fc4d93
NikitaMalofeev <Front.Work3@gmail.com>
2023-03-29 13:05:43 +0300

    проверка гита

---
+++ js/slider.js
@@ -0,0 +1,62 @@
+let images = [{
+    url: "https://wampi.ru/image/RpvMaOi",
+    title: "Mermeid and waterfall"
+}, {
+    url: "https://wampi.ru/image/Rpvixvx",
+    title: "Queen of autumn"
+}, {
+    url: "https://wampi.ru/image/Rpvim1O",
+    title: "Cyberpank girl"
+}, {
+    url: "https://wampi.ru/image/Rpvina6",
+    title: "Girl in the church"
+}, {
+    url: "https://wampi.ru/image/RpviCss",
+    title: "Violinist on the square"
+}];
+
+
+function initSlider () {
+    if (!images || !images.length) return;
+
+    let sliderImages = document.querySelector('.swiper__content');
+    let sliderArrows = document.querySelector('.button');
+
+    
+    initImages();
+    initArrows();
+
+    function initImages () {
+        images.forEach((image, index) => {
+            let imageDiv = `<div class="image n${index} ${index === 0? 'active' : ''}" style="background-image:url(${images[index].url});" data-index ="${index}"></div>`;
+            sliderImages.innerHTML += imageDiv;
+        });
+    }
+
+
+    function initArrows () {
+        sliderArrows.querySelectorAll('.button').forEach(arrow => {
+            arrow.addEventListener('click', function() {
+                let curImageNum = +sliderImages.querySelector('.active').dataset.index;
+                let nextImageNum;
+
+                if (arrow.classList.contains("button__prev")) {
+                    nextImageNum = curImageNum === 0? images.length - 1 : curImageNum - 1;
+                } else {
+                    nextImageNum = curImageNum === images.length? 0 : curImageNum + 1;
+                }
+                moveSlider(nextImageNum);
+            });
+        });
+    }
+
+    function moveSlider (num) {
+        sliderImages.querySelector(".active").classList.remove('active');
+        sliderImages.querySelector('.n' + num).classList.add('active')
+    } 
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", initSlider());
