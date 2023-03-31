55d0cd03b32005eabbb1a3381c293d01136f96d7
NikitaMalofeev <Front.Work3@gmail.com>
2023-03-31 23:27:48 +0300

    Почти рабочий и уже красивый слайдер с локальными асетами

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,17 @@
 let images = [{
-    url: "https://wampi.ru/image/RpvMaOi",
+    url: "../asset/item1.jpg",
     title: "Mermeid and waterfall"
 }, {
-    url: "https://wampi.ru/image/Rpvixvx",
+    url: "../asset/item2.jpg",
     title: "Queen of autumn"
 }, {
-    url: "https://wampi.ru/image/Rpvim1O",
+    url: "../asset/item3.jpg",
     title: "Cyberpank girl"
 }, {
-    url: "https://wampi.ru/image/Rpvina6",
+    url: "../asset/item4.jpg",
     title: "Girl in the church"
 }, {
-    url: "https://wampi.ru/image/RpviCss",
+    url: "../asset/item5.jpg",
     title: "Violinist on the square"
 }];
 
@@ -28,7 +28,7 @@ function initSlider () {
 
     function initImages () {
         images.forEach((image, index) => {
-            let imageDiv = `<div class="image n${index} ${index === 0? 'active' : ''}" style="background-image:url(${images[index].url});" data-index ="${index}"></div>`;
+            let imageDiv = `<div class="image n${index} ${index === 0? 'active' : ''}" style="background-image:url(${images[index].url})"; data-index ="${index}"></div>`;
             sliderImages.innerHTML += imageDiv;
         });
     }
@@ -40,10 +40,10 @@ function initSlider () {
                 let curImageNum = +sliderImages.querySelector('.active').dataset.index;
                 let nextImageNum;
 
-                if (arrow.classList.contains("button__prev")) {
+                if (arrow.classList.contains("button-prev")) {
                     nextImageNum = curImageNum === 0? images.length - 1 : curImageNum - 1;
                 } else {
-                    nextImageNum = curImageNum === images.length? 0 : curImageNum + 1;
+                    nextImageNum = curImageNum === images.length - 1? 0 : curImageNum + 1;
                 }
                 moveSlider(nextImageNum);
             });
@@ -58,5 +58,4 @@ function initSlider () {
 
 
 
-
-document.addEventListener("DOMContentLoaded", initSlider());
+document.addEventListener("DOMContentLoaded", initSlider);
